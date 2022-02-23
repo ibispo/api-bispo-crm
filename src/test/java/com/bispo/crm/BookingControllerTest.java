c8e8f2306b92057a5ede95c74f0f02a54c2cd628
israel bispo <imbispo@gmail.com>
2022-02-22 21:34:07 -0300

    Ajustes gerais

--- src/test/java/com/bispo/crm/BookingControllerTest.java
+++ src/test/java/com/bispo/crm/BookingControllerTest.java
@@ -1,6 +1,7 @@
 package com.bispo.crm;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
@@ -14,7 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.bispo.crm.model.Booking;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
